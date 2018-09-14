f9bc2b1308a233582452cc15fa7dd160eefe4a94
AlbertoCota93 <is694734@iteso.mx>
2018-09-13 23:19:46 -0500

    done'd

--- app/src/main/java/com/iteso/tarea2/ActivityMain.java
+++ app/src/main/java/com/iteso/tarea2/ActivityMain.java
@@ -1,5 +1,6 @@
 package com.iteso.tarea2;
 
+import android.os.PersistableBundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -16,13 +17,14 @@ import android.widget.ToggleButton;
 
 public class ActivityMain extends AppCompatActivity {
 
+    private final static String Button_State="";
     Button m;
     Button s;
     Button l;
     Button xl;
     ImageButton like;
     Button cart;
-
+    String cartString="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +38,8 @@ public class ActivityMain extends AppCompatActivity {
         like=findViewById(R.id.activity_main_like_button);
         cart=findViewById(R.id.activity_main_cart);
        final CoordinatorLayout coordinatorLayout= findViewById(R.id.activity_main_coordinator);
+
+
         like.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,24 +50,48 @@ public class ActivityMain extends AppCompatActivity {
             }
         });
 
-        cart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (cart.getText().toString().equals(R.string.cartButtonPressed)) {
-                    Toast toast = Toast.makeText(ActivityMain.this,getString(R.string.cartButtonPressed_again),Toast.LENGTH_LONG);
-                    toast.setGravity(Gravity.CENTER,0,0);
-                    toast.show();
-                } else {
-                    cart.setText(R.string.cartButtonPressed);
-                    Snackbar.make(coordinatorLayout, getString(R.string.cartButtonPressed), Snackbar.LENGTH_INDEFINITE).
-                            setAction("UNDO", new View.OnClickListener() {
-                                @Override
-                                public void onClick(View v) {
-                                    cart.setText(R.string.cartButton);
-                                }
-                            }).show();
+            if (cartString.equals(R.string.cartButtonPressed)){
+                cart.setText(R.string.cartButtonPressed);
+                Snackbar.make(coordinatorLayout, getString(R.string.cartButtonPressed), Snackbar.LENGTH_INDEFINITE).
+                        setAction("UNDO", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                cart.setText(R.string.cartButton);
+                            }
+                        }).show();
+            }else {
+                cart.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (cart.getText().toString().equals(R.string.cartButtonPressed)) {
+                            Toast toast = Toast.makeText(ActivityMain.this, getString(R.string.cartButtonPressed_again), Toast.LENGTH_LONG);
+                            toast.setGravity(Gravity.CENTER, 0, 0);
+                            toast.show();
+                        } else {
+                            cart.setText(R.string.cartButtonPressed);
+                            Snackbar.make(coordinatorLayout, getString(R.string.cartButtonPressed), Snackbar.LENGTH_INDEFINITE).
+                                    setAction("UNDO", new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            cart.setText(R.string.cartButton);
+                                        }
+                                    }).show();
+                        }
+                    }
+                });
             }
-        }
-    });
+       }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString(Button_State,cart.getText().toString());
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        cartString = savedInstanceState.getString(Button_State);
+        cart.setText(cartString);
     }
 }
